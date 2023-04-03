3a28dca96ec5f1af6f971f6930c3c0bea8bcd3df
KumaShow <kumadth@gmail.com>
2023-04-03 20:55:55 +0800

    更新上傳檔案樣式

--- src/components/UploadNew.vue
+++ src/components/UploadNew.vue
@@ -1,14 +1,17 @@
 <script>
 import moment from 'moment';
-import { mapActions } from 'pinia';
+import { mapActions, mapState, mapWritableState } from 'pinia';
 import useFileStore from '../stores';
 
 export default {
-  // emits: ['upload'],
   data() {
     return {
       time: '',
     };
+  },
+  computed: {
+    ...mapState(useFileStore, ['isMobile']),
+    ...mapWritableState(useFileStore, ['signStep']),
   },
   methods: {
     ...mapActions(useFileStore, ['uploadPDF']),
@@ -27,22 +30,35 @@ export default {
 
 <template>
   <div
-      class="p-2.5 bg-white grow rounded-b-[15px]"
+    class="p-2.5 md:p-[30px] bg-white grow rounded-b-[15px]
+    h-full"
+  >
+    <label
+      class="border border-dashed border-gray-dark rounded-[15px] h-full flex
+      items-center justify-center hover:bg-gray-light"
     >
-      <label class="border border-dashed rounded-[15px] h-full flex items-center justify-center">
-        <div class="text-center text-gray-dark">
-          <p class="mb-3.5">點擊此處上傳</p>
-          <img class="mb-3.5" src="@/assets/images/icon-document-pdf.png" alt="PDF icon">
-          <p>(限10MB以下PDF檔)</p>
-          <input
-            @change="upload"
-            class="hidden"
-            accept="application/pdf"
-            placeholder="選擇PDF檔案"
-            type="file"
-            ref="fileInput"
-          />
-        </div>
-      </label>
-    </div>
+      <div
+        class="text-center text-gray-dark text-10 sm:text-16 md:text-20"
+      >
+        <p class="mb-3.5">
+          點擊此處上傳
+          <span v-if="!isMobile"> 或 直接拖曳檔案</span>
+        </p>
+        <img
+          class="mb-3.5 mx-auto"
+          alt="PDF icon"
+          src="@/assets/images/icon-document-pdf.png"
+        >
+        <p>(限10MB以下PDF檔)</p>
+        <input
+          @change="upload"
+          class="hidden"
+          accept="application/pdf"
+          placeholder="選擇PDF檔案"
+          type="file"
+          ref="fileInput"
+        />
+      </div>
+    </label>
+  </div>
 </template>
