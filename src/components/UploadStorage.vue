1f6177752bad6ac70f214257accde6e221217671
KumaShow <kumadth@gmail.com>
2023-04-03 20:56:22 +0800

    更新已上傳區樣式

--- src/components/UploadStorage.vue
+++ src/components/UploadStorage.vue
@@ -3,15 +3,8 @@ import { mapState, mapWritableState } from 'pinia';
 import useFileStore from '../stores';
 
 export default {
-  // emits: ['upload'],
-  // props: {
-  //   pdfList: {
-  //     type: Array,
-  //   },
-  // },
   data() {
     return {
-      // testList: ['六角學院2022版活動', '廠商合約.pdf'],
       storageList: [
         {
           data: {
@@ -25,13 +18,12 @@ export default {
   },
   computed: {
     ...mapState(useFileStore, ['pdfList']),
-    ...mapWritableState(useFileStore, ['targetPDF']),
+    ...mapWritableState(useFileStore, ['targetPDF', 'signStep']),
   },
   methods: {
     getPDF(id) {
       this.selected = id;
       const file = this.pdfList.filter((item) => item.id === id);
-      // this.targetPDF = file[0].typedarray;
       this.targetPDF = file;
     },
   },
@@ -42,29 +34,32 @@ export default {
 </script>
 
 <template>
-  <section class="p-2.5 bg-white h-full rounded-[15px] text-10">
+  <section class="p-2.5 bg-white h-full rounded-b-[15px] text-10">
     <div>
       <div class="relative">
         <input
-        class="rounded-full border w-full py-2 px-4 mb-[5px]
-          placeholder-[#d9d9d9] focus:border-gray-dark focus:outline-none"
-        placeholder="搜尋文件名稱"
-        id="search"
-        type="text"
+          class="rounded-full border w-full py-2 px-4 mb-[5px]
+          text-10 sm:text-12 md:text-16 placeholder-[#d9d9d9]
+          focus:border-gray-dark focus:outline-none"
+          placeholder="搜尋文件名稱"
+          id="search"
+          type="text"
         />
         <label class="material-symbols-outlined text-[#d9d9d9]
         absolute top-2 right-3 text-20" for="search">
           search
         </label>
       </div>
-      <div class="px-4 mb-[5px] flex text-gray-dark">
+      <div class="px-4 mb-[5px] flex text-gray-dark text-10
+        sm:text-16 md:text-20"
+      >
         <p class="w-1/2">名稱</p>
         <p class="w-1/2">上傳時間</p>
       </div>
     </div>
     <ul>
       <template v-for="(item) in storageList" :key="item.id">
-        <li class="">
+        <li>
           <a
             @click.prevent="getPDF(item.id)"
             class="flex hover:bg-light-main
