4401c807d775c7d4ee292104522db3f838cdcc87
KumaShow <kumadth@gmail.com>
2023-04-03 21:01:08 +0800

    更新程式碼顯示順序

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -1,52 +1,3 @@
-<template>
-  <main class="p-2.5 bg-gray-light h-[calc(100%-76px-60px-20px)] flex flex-col">
-    <!-- 上傳 tab -->
-    <div class="flex flex-none bg-light-main">
-      <a
-        @click.prevent="() => setCurrentTab('UploadNew')"
-        class="w-1/2 text-center text-secondary
-        active:text-primary hover:text-primary bg-white py-2
-        rounded-t-[15px] shadow-[8px_4px_4px_0_rgba(238,237,232,0.5)]
-        cursor-pointer"
-      >
-        上傳新文件
-      </a>
-      <a
-        @click.prevent="() => setCurrentTab('UploadStorage')"
-        class="w-1/2 text-center text-secondary active:text-primary
-        hover:text-primary active:primary rounded-t-[15px] py-2
-        cursor-pointer"
-      >
-        選擇已上傳文件
-      </a>
-    </div>
-    <!-- 上傳區域 -->
-    <!-- 上傳新文件 -->
-    <component
-      :is="currentTab"
-      />
-      <!-- :pdf-list="pdfList" -->
-      <!-- @upload="uploadPDF" -->
-    <!-- 選擇已上傳文件 -->
-  </main>
-  <!-- step -->
-  <PDFStep />
-  <!-- 檔案大小提示 -->
-  <div class="fixed top-0 left-0 w-full h-full bg-dark/20 hidden" ref="modal">
-    <div class="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]
-    bg-white text-dark h-[154px] flex items-center justify-center
-    rounded-[15px] container"
-    >
-      <p>您的檔案太大了!</p>
-      <button type="button" class="rounded-[15px] py-1.5 px-3
-            absolute bottom-2.5 right-2.5 bg-secondary text-white"
-            @click="closeModal">
-        我知道了
-      </button>
-    </div>
-  </div>
-</template>
-
 <script>
 import { mapState } from 'pinia';
 import PDFStep from '../components/PDFStep.vue';
@@ -65,17 +16,10 @@ export default {
   data() {
     return {
       modal: null,
-      // uploadState: 'new',
-      // pdfList: [],
       currentTab: 'UploadNew',
     };
   },
   watch: {
-    // uploadState() {
-    //   if (this.uploadState === 'new') {
-    //     // console.log();
-    //   }
-    // },
     isFileOverSize() {
       if (this.isFileOverSize) {
         this.modal.classList.remove('hidden');
@@ -84,32 +28,38 @@ export default {
   },
   computed: {
     ...mapState(useFileStore, ['isFileOverSize']),
+
+    activeTab() {
+      return this.currentTab === 'UploadNew'
+        ? 'UploadNew'
+        : 'UploadStorage';
+    },
   },
   methods: {
-    // ...mapActions(useFileStore, ['uploadPDF']),
-    // uploadPDF(data) {
-    //   if (data.file === undefined) return;
-    //   if (data.file.size > 10485760) {
-    //     this.modal.classList.remove('hidden');
-    //   } else {
-    //     // 產生 fileReader 物件
-    //     const fileReader = new FileReader();
-
-    //     fileReader.readAsArrayBuffer(data.file);
-    //     fileReader.addEventListener('load', () => {
-    //     // 獲取 readAsArrayBuffer 產生的結果，並用來渲染 PDF
-    //       const typedarray = new Uint8Array(fileReader.result);
-    //       // renderPDF(typedarray);
-    //       // console.log(typedarray);
-    //       this.pdfList.push({
-    //         data,
-    //         typedarray,
-    //       });
-    //     });
-    //   }
-    // },
     setCurrentTab(tab) {
       this.currentTab = tab;
+    },
+    isActive(tab) {
+      return this.activeTab === tab;
+    },
+    getTabClass(tab) {
+      const isActive = this.isActive(tab);
+      return {
+        'w-1/2': true,
+        'text-center': true,
+        'py-2': true,
+        'rounded-t-[15px]': true,
+        'cursor-pointer': true,
+        'transition-colors': true,
+        'text-10': true,
+        'sm:text-16': true,
+        'md:text-20': true,
+        'text-primary': isActive,
+        'text-secondary': !isActive,
+        'bg-white': isActive,
+        'shadow-[8px_4px_4px_0_rgba(238,237,232,0.5)]': isActive && tab === 'UploadNew',
+        'shadow-[-8px_4px_4px_0_rgba(238,237,232,0.5)]': isActive && tab === 'UploadStorage',
+      };
     },
     closeModal() {
       this.modal.classList.add('hidden');
@@ -120,3 +70,51 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div class="flex flex-col h-[calc(100%-60px)]">
+    <main class="p-2.5 bg-gray flex flex-col flex-1
+      pb-[144px] sm:pb-[94px] md:pb-[108px]"
+    >
+      <!-- 上傳 tab -->
+      <div class="md:container w-full">
+        <div class="flex flex-none bg-light-main rounded-t-[15px]"
+        >
+          <a
+            @click.prevent="setCurrentTab('UploadNew')"
+            :class="getTabClass('UploadNew')"
+          >
+            上傳新文件
+          </a>
+          <a
+            @click.prevent="setCurrentTab('UploadStorage')"
+            :class="getTabClass('UploadStorage')"
+          >
+            選擇已上傳文件
+          </a>
+        </div>
+      </div>
+      <div class="md:container w-full h-full">
+        <component :is="currentTab" />
+      </div>
+    </main>
+    <!-- step -->
+    <PDFStep />
+  </div>
+  <!-- 檔案大小提示 -->
+  <div class="fixed top-0 left-0 w-full h-full bg-dark/20 hidden" ref="modal">
+    <div class="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]
+    bg-white text-dark h-[154px] flex items-center justify-center
+    rounded-[15px] container"
+    >
+      <p>您的檔案太大了!</p>
+      <button
+        type="button"
+        class="rounded-[15px] py-1.5 px-3 absolute bottom-2.5 right-2.5 bg-secondary text-white"
+        @click="closeModal"
+      >
+        我知道了
+      </button>
+    </div>
+  </div>
+</template>
