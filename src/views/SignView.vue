a601540556d4d24da022e7c5a6d2b99566d6dd97
KumaShow <kumadth@gmail.com>
2023-04-03 21:00:53 +0800

    更新程式碼顯示順序

--- src/views/SignView.vue
+++ src/views/SignView.vue
@@ -1,56 +1,29 @@
-<template>
-  <div class="flex flex-col h-[calc(100%-60px-76px-20px)]">
-    <!-- <input type="file" class="select" accept="application/pdf" @change="test($event)" /> -->
-    <!-- <button class="p-4 border" @click="test">click</button>
-    <button class="px-4" @click="increaseSize">+</button>
-    <button class="px-4" @click="decreaseSize">-</button> -->
-    <div class="grow overflow-scroll">
-      <canvas class="p-4 h-full w-full" ref="canvasPDF" id="canvasPDF"></canvas>
-    </div>
-    <!-- 簽名功能 -->
-    <div class="w-full shadow-[0_-4px_4px_0px_rgba(0,0,0,0.2)]">
-      <button
-        @click="createSign"
-        class="w-1/2 inline-flex justify-center border-r-2 py-1.5 hover:bg-light-main"
-        type="button"
-      >
-        創建簽名 <img src="@/assets/images/icon-pen.png" alt="手寫簽名" class="inline ml-[5px]">
-      </button>
-      <button class="w-1/2 inline-flex justify-center py-1.5 hover:bg-light-main"> 創建簽名 <img
-          src="@/assets/images/icon-picture.png" alt="圖片簽名" class="inline ml-[5px]">
-      </button>
-    </div>
-  </div>
-  <SignNameModal ref="signModal" />
-  <PDFStep />
-</template>
-
 <script>
 import { GlobalWorkerOptions, getDocument } from 'pdfjs-dist/build/pdf';
 import { fabric } from 'fabric';
 import { mapState, mapWritableState } from 'pinia';
 import useFileStore from '../stores';
 import PDFStep from '../components/PDFStep.vue';
-import SignNameModal from './SignNameModal.vue';
+import SignNameAreaMobile from '../components/SignNameAreaMobile.vue';
+import SignNameAreaPC from '../components/SignNameAreaPC.vue';
 
 export default {
   components: {
     PDFStep,
-    SignNameModal,
+    SignNameAreaMobile,
+    SignNameAreaPC,
   },
   data() {
     return {
       scale: 1,
-      // PDFFile: null,
       viewport: 0,
       Base64Prefix: null,
-      // canvasPDF: null,
       context: null,
     };
   },
   computed: {
     ...mapState(useFileStore, ['pdfList', 'targetPDF']),
-    ...mapWritableState(useFileStore, ['canvasPDF']),
+    ...mapWritableState(useFileStore, ['canvasPDF', 'finalPDF', 'signStep']),
   },
   watch: {
     // scale() {
@@ -58,21 +31,20 @@ export default {
     // },
   },
   mounted() {
-    // this.canvasPDF = this.$refs.canvasPDF;
     this.Base64Prefix = 'data:application/pdf;base64,';
     GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js';
-    // this.context = this.canvasPDF.getContext('2d');
+
     if (!this.targetPDF) {
       this.$router.push('/upload');
     } else {
       this.onFileChange(this.targetPDF[0].data.file);
-      // this.renderPDF(this.targetPDF);
-      // console.log(this.targetPDF[0].data.file);
     }
   },
   methods: {
-    createSign() {
-      this.$refs.signModal.openModal();
+    createFinal() {
+      this.signStep = 'final';
+      this.finalPDF = this.canvasPDF.toDataURL('image/png');
+      this.$router.push('/final');
     },
 
     // async renderPDF(data) {
@@ -149,7 +121,9 @@ export default {
       });
     },
     async onFileChange(file) {
-      this.canvasPDF = new fabric.Canvas('canvasPDF');
+      this.canvasPDF = new fabric.Canvas('canvasPDF', {
+        selection: true,
+      });
       this.canvasPDF.requestRenderAll();
       const pdfData = await this.printPDF(file);
       const pdfImage = await this.pdfToImage(pdfData);
@@ -159,8 +133,41 @@ export default {
       this.canvasPDF.setHeight(pdfImage.height / window.devicePixelRatio);
 
       // 將 PDF 畫面設定為背景
-      this.canvasPDF.setBackgroundImage(pdfImage, this.canvasPDF.renderAll.bind(this.canvasPDF));
+      this.canvasPDF.setBackgroundImage(
+        pdfImage,
+        this.canvasPDF.renderAll.bind(this.canvasPDF),
+      );
     },
   },
 };
 </script>
+
+<template>
+  <div class="flex flex-col md:flex-row h-[calc(100%-60px-117px-48px)]
+  sm:h-[calc(100%-60px-75px-48px)]
+  md:h-[calc(100%-60px-82px)] bg-gray">
+    <!-- <input type="file" class="select" accept="application/pdf" @change="test($event)" /> -->
+    <!-- <button class="p-4 border" @click="test">click</button>
+    <button class="px-4" @click="increaseSize">+</button>
+    <button class="px-4" @click="decreaseSize">-</button> -->
+    <!-- 簽名功能 - PC -->
+    <div class="w-3/12">
+      <SignNameAreaPC />
+    </div>
+    <!-- canvas -->
+    <div class="flex-1 overflow-auto md:w-9/12 p-2">
+      <div class="overflow-auto border-2 h-full">
+        <canvas ref="canvasPDF" id="canvasPDF"></canvas>
+      </div>
+    </div>
+    <!-- 簽名功能 - 行動裝置 -->
+    <SignNameAreaMobile />
+  </div>
+  <PDFStep @create="createFinal" />
+</template>
+
+<style>
+.sign-enter-active {
+  transition: all 0.3s ease-in;
+}
+</style>
